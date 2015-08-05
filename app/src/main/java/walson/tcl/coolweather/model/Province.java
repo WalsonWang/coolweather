00756f672dd42bb42a38b50e44a9f36542d22baf
Walson <xiangfei.wang@tcl.com>
2015-08-05 17:21:27 +0800

    新增数据库帮助类，以及个表对应的实体类

---
+++ app/src/main/java/walson/tcl/coolweather/model/Province.java
@@ -0,0 +1,34 @@
+package walson.tcl.coolweather.model;
+
+/**
+ * Created by 93766 on 8/5/2015.
+ */
+public class Province {
+    private int id;
+    private String provinceName;
+    private String provinceCode;
+
+    public String getProvinceName() {
+        return provinceName;
+    }
+
+    public void setProvinceName(String provinceName) {
+        this.provinceName = provinceName;
+    }
+
+    public String getProvinceCode() {
+        return provinceCode;
+    }
+
+    public void setProvinceCode(String provinceCode) {
+        this.provinceCode = provinceCode;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+}
